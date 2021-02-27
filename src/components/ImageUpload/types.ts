894d92d80ab85642a400484fc4c08c78957dcc41
pfdzm <pfdzm@users.noreply.github.com>
2021-02-27 16:21:29 +0100

    extend types to include base64 preview image

--- src/components/ImageUpload/types.ts
+++ src/components/ImageUpload/types.ts
@@ -1,6 +1,7 @@
 export interface Image {
   file: File
   status: ProgressStatus
+  imgUrl: string
 }
 
 type ProgressStatus = 'in-progress' | 'done' | 'failed'
@@ -10,6 +11,7 @@ export type State = Map<string, Image>
 export type Action =
   | { type: 'add_image'; file: File; status: ProgressStatus; uuid: string }
   | { type: 'update_status'; status: ProgressStatus; uuid: string }
+  | { type: 'update_img_url'; imgUrl: string; uuid: string }
 
 export interface ImageProps {
   img: Image
